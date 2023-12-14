9075fad2bcb03bb4be47c3cf0f3edc2e876446fb
anton.v <anton.v@northitgroup.eu>
2023-12-14 16:56:20 +0300

    Initial commit

---
+++ src/Controllers/HealthCheckController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Gedisa\Healthcheck\Controllers;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Route;
+
+class HealthCheckController
+{
+    public function healthCheck(): JsonResponse
+    {
+        return response()->json(
+            $status = collect([
+                'redis' => $this->redisStatus(),
+                'database' => $this->databaseStatus(),
+                'sentry' => $this->sentryStatus(),
+            ]),
+            status: $status->every(fn ($item) => ($item))
+                ? 200
+                : 503
+        );
+    }
+
+    private function redisStatus(): bool
+    {
+        $testKey = '_health';
+        $testValue = 'ok';
+
+        try {
+            Redis::connection();
+
+            Redis::set($testKey, $testValue);
+
+            return Redis::get($testKey) === $testValue;
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+
+            return false;
+        }
+    }
+
+    private function databaseStatus(): bool
+    {
+        try {
+            DB::connection()->getPdo();
+
+            return DB::connection()->getDatabaseName() === config('database.connections.mysql.database');
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+
+            return false;
+        }
+    }
+
+    private function sentryStatus(): bool
+    {
+        try {
+            return Route::dispatch(Request::create(config('sentry.dsn')))->getStatusCode() === 200;
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+
+            return false;
+        }
+    }
+}
